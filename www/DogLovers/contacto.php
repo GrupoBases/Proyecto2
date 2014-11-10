8f1cd6fdd82fab6e074ec146e74b72f7988a0e86
Jef7 <jecam94@hotmail.com>
2014-11-09 23:42:15 -0600

    pantalla contacto

---
+++ www/DogLovers/contacto.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang='es'>
+    <head>
+        <meta charset="UTF-8"/> 
+        <title>
+            Dog Lovers
+        </title>
+        
+        <link rel="shortcut icon" type="image/x-icon" href="graficos/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="css/contacto.css" />
+    </head>
+       
+    <body>
+        <div class="estructuraForm">
+            <form>      
+                <div class="header">
+                    <img id = "logo" src="graficos/logo1.png";
+                </div>
+                    <div class="contenedor">         
+                        <label for="campo obligatorio"> *Campo Obligatorio:</label>
+                        
+                        <h1 class="tagInfo"> Información <span class="triangulo"</span></h1>
+                        
+                        <div id="casaCuna">
+                            <div id="info">
+                                <h1 style="color:white"> Proyecto II de Bases de Datos I</h1>
+                                <h2 style="color:white"> Desarrollado por:</h2>
+                                <h3 style="color:white"> - Jeffrey Camareno Fonseca</h3>
+                                <h3 style="color:white"> - Olman Quirós Jiménez</h3>
+                                <h3 style="color:white"> - Cristiam Flores Núñez </h3>
+                            </div>
+                        </div>
+                        
+                        <h1 class="tagComentarios"> Sugerencias <span class="triangulo"</span></h1>
+                        
+                        <label for="comentarios"> * Sugerencias o comentarios:</label>
+                        <textarea id="comentarios" placeholder="Escribe tus sugerencias o comentarios"></textarea>
+
+
+                        <input type="submit" value="Enviar">
+                        <input type="submit" value="Cancelar">  
+                    </div>
+            </form>  
+        </div>
+    </body>
+</html>
