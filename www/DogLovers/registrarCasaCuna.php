3da0628275cc1131517e25607d3246eaffbb9201
cristiam <stevenflores88@hotmail.com>
2014-11-10 14:37:52 -0600

    REGISTROSLISTOS CRISTIAM

--- www/DogLovers/registrarCasaCuna.php
+++ www/DogLovers/registrarCasaCuna.php
@@ -16,37 +16,100 @@
             if (!array_key_exists('userName', $_SESSION)) {
                 header('Location: login.php');
             }
+
+            include 'Conexion.php';
+            include 'funcionalidad.php';
+            $conexion=new Conexion();
+            $conn=$conexion->conectar();
+
+        $tipoMascotaError = $tamanoError = $validacionError="";
+         $tamano =$tipoMascota ="";
+        if ($_SERVER["REQUEST_METHOD"] == "POST" and ($_POST['register'])) { //Si estamos recibiendo datos nuevos verificarlos
+            if(empty($_POST["tipoMascota"]) or empty($_POST["tamano"])) {
+                if(empty($_POST["tipoMascota"])){
+                            $tipoMascotaError = "*Tipo de mascota requerido";  //si el username no se encuentra mostrar error
+                }
+                if (empty($_POST["tamano"])) {
+                            $tamanoError = "*Tamaño requerido"; //si la pass no se encuentra mostrar error
+                }
+            }//if empty
+            else{
+                $idUsuario =$_SESSION["id_user"];
+                $tamano = test_input($_POST["tamano"]);    // si si esta verificarlo 
+                $tipoMascota = test_input($_POST["tipoMascota"]); //si si esta verifi
+                $donacion = test_input($_POST["pregunta"]); //si si esta verifi
+                $idDireccion = -1;
+                
+                $stid = oci_parse($conn, 'begin :m := insertarCasaCuna(:a,:b,:c,:d); end;'); 
+                oci_bind_by_name($stid, ':a', $idUsuario);
+                oci_bind_by_name($stid, ':b', $tipoMascota);
+                oci_bind_by_name($stid, ':c', $tamano);
+                oci_bind_by_name($stid, ':d', $donacion);
+                oci_bind_by_name($stid, ':m', $idDireccion);
+                oci_execute($stid);
+                
+                 if($idDireccion != -1){    //si no retorna null pasar 
+                    header('Location: index.php');       
+                }else{
+                    $validacionError = "Hubo un error en el registro vuelva a registrarse";   
+                }
+            }// else empty
+        }//if principal
+        else if ($_SERVER["REQUEST_METHOD"] == "POST" and ($_POST['cancelar'])) {
+                    header('Location: index.php');  
+        } // else if 
+        
         ?>
         
         <div class="estructuraForm">
-            <form>      
+            <form name="registrarMascotaEncontrada_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  
+                  enctype="multipart/form-data">     
                 <div class="header">
                     <img id = "logo" src="graficos/logo1.png";
                 </div>
                     <div class="contenedor">  
                         
+                        <span class="error"> <?php echo $validacionError;?></span>
                         <label for="Tipo de Mascota"> Tipo de mascota:</label>
-                        <select id="tipoMascota">
-                            <option value=""> --elejir tipo mascota-- </option>
+                        <select name = "tipoMascota"id="tipoMascota">
+                            <option value=""> ---- </option>
+                             <?php
+                                $stid = oci_parse($conn, 'SELECT NOMBRETIPOMASCOTA FROM TBTIPOMASCOTA'); 
+                                oci_execute($stid);
+                                while(($row = oci_fetch_array($stid, OCI_BOTH))!= false) {
+                                    echo utf8_encode('<option value='.$row['NOMBRETIPOMASCOTA'].'>' . $row['NOMBRETIPOMASCOTA'] . '</option>');
+                                }
+                            ?>
                         </select>
+                        <br>
+                        <span class="error"> <?php echo $tipoMascotaError;?></span>
                         
                         <label for="Tamano de Mascota"> Tamaño de mascota:</label>
-                        <select id="tamanoMascota">
-                            <option value=""> --elejir tamaño mascota-- </option>
+                        <select name="tamano"id="tamanoMascota">
+                            <option value=""> ---- </option>
+                             <?php
+                                $stid = oci_parse($conn, 'SELECT NOMBRETAMANO FROM TBTAMANOMASCOTA'); 
+                                oci_execute($stid);
+                                while(($row = oci_fetch_array($stid, OCI_BOTH))!= false) {
+                                    echo utf8_encode('<option value='.$row['NOMBRETAMANO'].'>' . $row['NOMBRETAMANO'] . '</option>');
+                                }
+                            ?> 
                         </select>
+                        <br>
+                        <span class="error"> <?php echo $tamanoError;?></span> 
                         
                         <div class="pregunta">
                             <font color="white"> ¿Requiere donación para alimento?</font> 
                             <br />
-                            <input type="radio" name="si" id="si" value="si" > <font color="white">Sí</font> 
+                            <input type="radio" name="pregunta" id="si" value="SI" > <font color="white">Sí</font> 
                             <br />
-                            <input type="radio" name="no" id="no" value="no"> <font color="white">No</font>
+                            <input type="radio" name="pregunta" id="no" value="NO"> <font color="white">No</font>
                             <br />
                         </div>
 
 
-                        <input type="submit"  class="registrar" value="Registrar">
-                        <input type="submit" class="cancelar" value="Cancelar">  
+                        <input type="submit"  name= "register"class="registrar" value="Registrar">
+                        <input type="submit" name = "cancelar"class="cancelar" value="Cancelar">  
                     </div>
             </form>  
         </div>
--- www/DogLovers/registrarDonaciones.php
+++ www/DogLovers/registrarDonaciones.php
@@ -11,25 +11,84 @@
     </head>
        
     <body>
+        <?php
+         session_start();    //comprobamos si el usuario ya inicio secion
+            if (!array_key_exists('userName', $_SESSION)) {
+                header('Location: login.php');
+            }
+
+            include 'Conexion.php';
+            include 'funcionalidad.php';
+            $conexion=new Conexion();
+            $conn=$conexion->conectar();
+    
+            $montoError = $asociacionError = $validacionError="";
+            $monto=$asociacion ="";
+            if ($_SERVER["REQUEST_METHOD"] == "POST" and ($_POST['register'])) { //Si estamos recibiendo datos nuevos verificarlos
+            if(empty($_POST["asociacion"]) or empty($_POST["monto"])) {
+                if(empty($_POST["asociacion"])){
+                            $asociacionError = "*Asociacion requerida";  //si el username no se encuentra mostrar error
+                }
+                if (empty($_POST["monto"])) {
+                            $montoError = "*Monto requerido"; //si la pass no se encuentra mostrar error
+                }
+            }//if empty
+            else{
+                $idUsuario = $_SESSION["id_user"];
+                $asociacion = test_input($_POST["asociacion"]);    // si si esta verificarlo 
+                $monto = test_input($_POST["monto"]); //si si esta verifi
+                $idDireccion = -1;
+                
+                $stid = oci_parse($conn, 'begin :m := insertardonacion(:a,:b,:c); end;'); 
+                oci_bind_by_name($stid, ':a', $idUsuario);
+                oci_bind_by_name($stid, ':b', $asociacion);
+                oci_bind_by_name($stid, ':c', $monto);
+                oci_bind_by_name($stid, ':m', $idDireccion);
+                oci_execute($stid);
+                
+                if($idDireccion != -1){    //si no retorna null pasar 
+                    header('Location: index.php');       
+                }else{
+                    $validacionError = "Hubo un error en el registro vuelva a registrarse";   
+                }
+            }// else empty
+        }//if principal
+        else if ($_SERVER["REQUEST_METHOD"] == "POST" and ($_POST['cancelar'])) {
+                    header('Location: index.php');  
+        } // else if 
+
+        ?>
         <div class="estructuraForm">
-            <form>      
+            <form name="registrarDonacion_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  
+                  enctype="multipart/form-data">          
                 <div class="header">
                     <img id = "logo" src="graficos/logo1.png";
                 </div>
                 </div>
                     <div class="contenedor">   
-                        
-                        <label for="Asociaciones"> Elige una asocioacón para donar:</label>
-                        <select id="Asociaciones"> 
-                            <option value=""> --asociaciones--</option>        
+                        <br>
+                        <span class="error"> <?php echo $validacionError;?></span> 
+                        <label for="Asociaciones"> Elige una asociación para donar:</label>
+                        <select name ="asociacion" id="Asociaciones"> 
+                            <option value=""> ----</option> 
+                             <?php
+                                $stid = oci_parse($conn, 'SELECT NOMBRE FROM TBASOCIACION'); 
+                                oci_execute($stid);
+                                while(($row = oci_fetch_array($stid, OCI_BOTH))!= false) {
+                                    echo utf8_encode('<option value='.$row['NOMBRE'].'>' . $row['NOMBRE'] . '</option>');
+                                }
+                            ?>
                         </select>
+                        <br>
+                        <span class="error"> <?php echo $asociacionError;?></span> 
                         
                         <label for="monto"> Monto a donar:</label>
-                        <input type="text" name="tel" value="" 
-                               onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;">  
+                        <input type="text" name="monto" id="monto"value="">  
+                        <br>
+                        <span class="error"> <?php echo $montoError;?></span> 
 
-                        <input type="submit" value="Donar">
-                        <input type="submit" value="Cancelar">
+                        <input type="submit" name="register"value="Donar">
+                        <input type="submit" name= "cancelar"value="Cancelar">
                     </div>
             </form>  
         </div>
