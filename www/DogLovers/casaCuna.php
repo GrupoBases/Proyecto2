020bc7c1c26cd354cfa38ebabe854837e4b7d770
cristiam <stevenflores88@hotmail.com>
2014-11-11 02:08:22 -0600

    casa cuna listo

--- www/DogLovers/casaCuna.php
+++ www/DogLovers/casaCuna.php
@@ -28,6 +28,12 @@
             $conexion=new Conexion();
             $conn=$conexion->conectar();
 
+             $sqlEstado = "select tbPersona.Nombrepersona,tbpersona.telefono,tbpersona.correo,tbcasacuna.donacion
+                            from tbpersona,tbcasacuna,tbmascota
+                            where tbcasacuna.estadocasacuna = 'CASACUNA' 
+                            and tbpersona.id_usuario = tbmascota.id_usuario 
+                            and tbcasacuna.id_usuario = tbpersona.id_usuario";
+
             if ($_SERVER["REQUEST_METHOD"] == "POST" and ($_POST['register'])) { //Si estamos recibiendo datos nuevos verificarlos
                 header('Location: registrarCasaCuna.php'); 
             }
@@ -44,12 +50,31 @@
                 </div>
                     <div class="contenedor" >  
                         
-                        <h1 class="tagCasaCuna"> Casas Cuna <span class="triangulo"</span></h1>
+                        <!--<h1 class="tagCasaCuna"> Casas Cuna <span class="triangulo"</span></h1>-->
                         
-                        <div id="casaCuna">
+                        <!--<div id="casaCuna">
                             <label > Soy una casa cuna</label>
-                        </div>
+                        </div>-->
                         
+                         <?php
+                                if (!empty($sqlEstado)) {
+                                    echo "<h1 class=\"tagCasaCuna\"> CASAS CUNA <span class=\"triangulo\"</span></h1>";
+                                $stid = oci_parse($conn, $sqlEstado); 
+                                oci_execute($stid);
+                                while(($row = oci_fetch_array($stid, OCI_BOTH))!= false) {
+                                    
+                                    echo "<div id=\"casaCuna\">
+                                    <p> NOMBRE:".$row['NOMBREPERSONA']."   <br>
+                                    TELÉFONO:".$row['TELEFONO']."   <br>
+                                    CORREO:".$row['CORREO']."   <br>
+                                    REQUIERE DONACION:".$row['DONACION']."<br>
+                            
+                                    </div>";
+                                    
+                                    
+                                }
+                                }
+                            ?>
                         <h1 class="tagRegistrar"> ¿Quieres ser Casa Cuna? <span class="triangulo"</span></h1>
 
 
